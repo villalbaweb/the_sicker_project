9b9c9883cbc3b9e99a19be9e1bae835f4a5f9d36
villalbaweb <villalbaweb@gmail.com>
2020-10-07 22:02:32 -0500

    Refcatoring to a FollowOnDistanceAttack script

---
+++ Assets/Scripts/Enemies/FollowOnDistanceAttacker.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using TheSicker.Core;
+using TheSicker.Player;
+
+namespace TheSicker.Enemies
+{
+    public class FollowOnDistanceAttacker : MonoBehaviour
+    {
+        // config
+        [SerializeField] float chaseDistance = 4.0f;
+
+        // Cache
+        PlayerMarker _player;
+        Follower _follower;
+
+        private void Awake()
+        {
+            _player = FindObjectOfType<PlayerMarker>();
+            _follower = GetComponent<Follower>();
+            _follower.target = _player.transform;
+        }
+
+        void Update()
+        {
+            Atack(IsInAttackRange());
+        }
+
+        private void Atack(bool isAttacking)
+        {
+            _follower.Following(isAttacking);
+        }
+
+        private bool IsInAttackRange()
+        {
+            return Vector2.Distance(_player.transform.position, transform.position) <= chaseDistance;
+        }
+
+        // called by Unity
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, chaseDistance);
+        }
+    }
+}
