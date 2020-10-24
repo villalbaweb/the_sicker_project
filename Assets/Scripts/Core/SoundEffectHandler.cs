1085df0b8b8c1e27873c4143ef529c887936c417
villalbaweb <villalbaweb@gmail.com>
2020-10-24 11:21:44 -0500

    Adding sound effect handler script to play sound fx on die

---
+++ Assets/Scripts/Core/SoundEffectHandler.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace TheSicker.Core
+{
+    public class SoundEffectHandler : MonoBehaviour
+    {
+        [SerializeField] AudioClip sfxAudio = null;
+        [SerializeField] float sfxAudioVolume = 0.5f;
+
+        public void StartSfx()
+        {
+            if (!sfxAudio) return;
+
+            AudioSource.PlayClipAtPoint(sfxAudio, Camera.main.transform.position, sfxAudioVolume);
+        }
+    }
+}
