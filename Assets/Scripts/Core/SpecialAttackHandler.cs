1db9e31903637f6d97d68eea3f9dfd1dad25a1c2
villalbaweb <villalbaweb@gmail.com>
2020-11-01 19:25:47 -0600

    Adding generic SpecialAttackHandler script

---
+++ Assets/Scripts/Core/SpecialAttackHandler.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+namespace TheSicker.Core
+{
+    public class SpecialAttackHandler : MonoBehaviour
+    {
+        // TODO: Add ISpecialAttack interface so it can be handled
+        // [SerializedField] ISpecialAttack specialAttack;
+        
+        // config
+        [SerializeField] float timeBetweenAttacks = 5f;
+
+        // cache
+        Animator _animator;
+
+        // state
+        float attackTimer = Mathf.Infinity;
+
+        void Awake()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Attack();
+        }
+
+        private void Attack()
+        {
+            attackTimer += Time.deltaTime;
+
+            if (attackTimer > timeBetweenAttacks)
+            {
+                // specialAttack.Attack();
+                
+                AnimatorTriggerAttack();
+
+                attackTimer = 0;
+            }
+        }
+
+        private void AnimatorTriggerAttack()
+        {
+            _animator.SetTrigger("Attack");
+        }
+    }
+}
