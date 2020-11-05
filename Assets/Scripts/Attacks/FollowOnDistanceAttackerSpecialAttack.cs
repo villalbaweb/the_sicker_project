4519b9fd65ed2f814c30d3195c66876a0eb488bc
villalbaweb <villalbaweb@gmail.com>
2020-11-05 07:37:11 -0600

    Adding FolloONDIstance enemy special attack to be a small clone of himself

--- Assets/Scripts/Attacks/FollowOnDistanceAttackerSpecialAttack.cs
+++ Assets/Scripts/Attacks/FollowOnDistanceAttackerSpecialAttack.cs
@@ -1,14 +1,22 @@
-﻿using UnityEngine;
+﻿using TheSicker.Core;
+using UnityEngine;
 
 namespace TheSicker.Attacks
 {
-
     public class FollowOnDistanceAttackerSpecialAttack : MonoBehaviour, ISpecialAttack
     {
+        // cache
+        ObjectPooler _objectPooler;
+
+        private void Awake()
+        {
+            _objectPooler = FindObjectOfType<ObjectPooler>();
+        }
+
         // This is the Attack implementation
         public void Attack()
         {
-            print("Special Attack here...");
+            _objectPooler.SpawnFromPool("FollowOnDistanceSmallAttacker", gameObject.transform.position, Quaternion.identity);
         }
     }
 }
