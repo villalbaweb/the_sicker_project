10ceee1447ff9fbacb1ce972f147d29fad6d1ebb
villalbaweb <villalbaweb@gmail.com>
2020-11-08 07:52:50 -0600

    Refactoring Health script, now it just triggers the unity event and added an additional DieHandler script that will react to it.

---
+++ Assets/Scripts/Core/DieHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace TheSicker.Core
+{
+    public class DieHandler : MonoBehaviour
+    {
+        // config
+        [SerializeField] float timeToDisableAfterDie = 1.5f;
+
+        // cache
+        Rigidbody2D _rigidBody2D;
+
+        private void Awake()
+        {
+            _rigidBody2D = GetComponent<Rigidbody2D>();
+        }
+
+        private IEnumerator DieCoroutine()
+        {
+            _rigidBody2D.simulated = false;
+
+            yield return new WaitForSecondsRealtime(timeToDisableAfterDie);
+
+            _rigidBody2D.simulated = true;
+            gameObject.SetActive(false);
+        }
+
+        public void Die()
+        {
+            StartCoroutine(DieCoroutine());
+        }
+    }
+}
--- Assets/Scripts/Core/Health.cs
+++ Assets/Scripts/Core/Health.cs
@@ -9,17 +9,12 @@ namespace TheSicker.Core
         // config
         [SerializeField] int health = 100;
         [SerializeField] UnityEvent onDieEvent = null;
-        [SerializeField] float timeToDisableAfterDie = 1.5f;
-
-        // cache
-        Rigidbody2D _rigidBody2D;
 
         // state
         int initialHealth;
 
         private void Awake() 
         {
-            _rigidBody2D = GetComponent<Rigidbody2D>();
             initialHealth = health;    
         }
 
@@ -29,26 +24,13 @@ namespace TheSicker.Core
 
             if(health <= Mathf.Epsilon) 
             {
-               StartCoroutine(Die());
+                onDieEvent.Invoke();
             }
         }
 
         public void RestoreInitialHealth()
         {
             health = initialHealth;
-        }
-
-        private IEnumerator Die()
-        {
-
-            onDieEvent.Invoke();
-            _rigidBody2D.simulated = false;
-
-            yield return new WaitForSecondsRealtime(timeToDisableAfterDie);
-
-            _rigidBody2D.simulated = true;
-            gameObject.SetActive(false);
-
         }
     }
 }
