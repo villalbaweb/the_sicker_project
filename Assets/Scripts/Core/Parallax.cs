c01fa90b7ceacbe6f64ebf36d5e57f4b8ec21f93
villalbaweb <villalbaweb@gmail.com>
2020-11-14 00:40:31 -0600

    Added up and down parallax effect

--- Assets/Scripts/Core/Parallax.cs
+++ Assets/Scripts/Core/Parallax.cs
@@ -8,28 +8,53 @@ namespace TheSicker.Core
         [SerializeField] GameObject camera;
         [SerializeField] float parallaxEffect;
 
-        private float length, startpos;
+        private float lengthX, lengthY, startposX, startposY;
 
         // Start is called before the first frame update
         void Start()
         {
-            startpos = transform.position.x;
-            length = GetComponent<SpriteRenderer>().bounds.size.x;
+            SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+
+            startposX = transform.position.x;
+            startposY = transform.position.y;
+            lengthX = renderer.bounds.size.x;
+            lengthY = renderer.bounds.size.y;
         }
 
         // Update is called once per frame
         void FixedUpdate()
         {
+            ProcessXPosition();
+            ProcessYPosition();
+        }
+
+        void ProcessXPosition()
+        {
             float temp = (camera.transform.position.x * (1 - parallaxEffect));
             float distance = (camera.transform.position.x * parallaxEffect);
-            transform.position = new Vector3(startpos + distance, transform.position.y, transform.position.z);
-            if (temp > startpos + length)
+            transform.position = new Vector3(startposX + distance, transform.position.y, transform.position.z);
+            if (temp > startposX + lengthX)
             {
-                startpos += length;
+                startposX += lengthX;
             }
-            else if (temp < startpos - length)
+            else if (temp < startposX - lengthX)
             {
-                startpos -= length;
+                startposX -= lengthX;
+            }
+        }
+
+        void ProcessYPosition()
+        {
+            float temp = (camera.transform.position.y * (1 - parallaxEffect));
+            float distance = (camera.transform.position.y * parallaxEffect);
+            transform.position = new Vector3(transform.position.x, startposY + distance, transform.position.z);
+            if (temp > startposY + lengthY)
+            {
+                startposY += lengthY;
+            }
+            else if (temp < startposY - lengthY)
+            {
+                startposY -= lengthY;
             }
         }
     }
