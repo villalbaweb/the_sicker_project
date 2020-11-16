3ffaa88854e1bab6761daf9057f222892f8cd1fa
villalbaweb <villalbaweb@gmail.com>
2020-11-15 21:23:34 -0600

    Adding PlanetMovementHandler script so we can move the planets in the background

---
+++ Assets/Scripts/Core/PlanetMovementHandler.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace TheSicker.Core
+{
+    public class PlanetMovementHandler : MonoBehaviour
+    {
+        // config
+        [Tooltip("Moving speed on Y axis in local space")]
+        [SerializeField] float speed = 0.1f;
+        [SerializeField] Vector3 direction = new Vector3();
+
+        //moving the object with the defined speed
+        private void Update()
+        {
+            transform.Translate(direction * speed * Time.deltaTime);
+        }
+    }
+}
