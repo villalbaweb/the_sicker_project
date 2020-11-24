cf75ec32a69f56a1265bf507275fcbd5f6066441
villalbaweb <villalbaweb@gmail.com>
2020-11-24 12:11:23 -0600

    Add Weapon Scriptable Object

---
+++ Assets/Scripts/Combat/Weapon.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace TheSicker.Projectile
+{
+    [CreateAssetMenu(fileName = "Weapon", menuName = "Weapons/Make New Weapon", order = 0)]
+    public class Weapon : ScriptableObject
+    {
+
+    }
+}
