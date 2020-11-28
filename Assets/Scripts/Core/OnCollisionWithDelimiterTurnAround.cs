631bb4abb8b84bacc18968b87c15a3d578958f6a
villalbaweb <villalbaweb@gmail.com>
2020-11-28 14:23:47 -0600

    Adding OnCollisionWithDelimiterTurnAround script to follower enemies so they can collide with PlayableArea delimiters and reacto by changing the direction each time

---
+++ Assets/Scripts/Core/OnCollisionWithDelimiterTurnAround.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace TheSicker.Core
+{
+    public class OnCollisionWithDelimiterTurnAround : MonoBehaviour
+    {
+        // config
+        [SerializeField] string tagToIdentifyAndTurn = "";
+
+        //Just hit another collider 2D
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if(collision.gameObject.tag != tagToIdentifyAndTurn) return;
+            
+            transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, Random.Range(45, 360));
+        }
+    }
+}
