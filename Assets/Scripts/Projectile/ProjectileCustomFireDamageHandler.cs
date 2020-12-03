d89b5658e64aeb59d41885ac9b7b431eea830831
villalbaweb <villalbaweb@gmail.com>
2020-12-02 23:30:24 -0600

    Refactoring so now the damage amount generated by the particle system (Custom Fire) is being  configured in the particle system, now we can have different amounts dor diff custom weapons

---
+++ Assets/Scripts/Projectile/ProjectileCustomFireDamageHandler.cs
@@ -0,0 +1,23 @@
+﻿using TheSicker.Core;
+using UnityEngine;
+
+namespace TheSicker.Projectile
+{
+    public class ProjectileCustomFireDamageHandler : MonoBehaviour
+    {
+        // config   
+        [SerializeField] int particleSystemDamage = 1000;
+
+        // cache
+        Health _health;
+
+        // This function is being trigger by any particle system with collision
+        // and send messages enable, other is the object receiving the impact
+        private void OnParticleCollision(GameObject other)
+        {
+            _health = other?.GetComponent<Health>();
+
+            _health?.TakeDamage(particleSystemDamage);
+        }
+    }
+}
