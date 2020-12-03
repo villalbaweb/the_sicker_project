bfcf313f1a89162f519ee3595f8d5f898e5efde9
villalbaweb <villalbaweb@gmail.com>
2020-12-02 23:15:12 -0600

    Adding EnemyDieOnParticleCollisionHandler to kill the enemy on particle collision ;-)

---
+++ Assets/Scripts/Enemies/EnemyDieOnParticleCollisionHandler.cs
@@ -0,0 +1,27 @@
+﻿using TheSicker.Core;
+using UnityEngine;
+
+namespace TheSicker.Enemies
+{
+    public class EnemyDieOnParticleCollisionHandler : MonoBehaviour
+    {
+        // config   
+        [SerializeField] int particleSystemDamage = 1000;
+
+        // cache
+        Health _health;
+
+        // Start is called before the first frame update
+        void Awake() 
+        {
+            _health = GetComponent<Health>();
+        }
+
+        // This function is being trigger by any particle system with collision
+        // and send messages enable, so now we can kill the player with particles
+        private void OnParticleCollision(GameObject other)
+        {
+            _health.TakeDamage(particleSystemDamage);
+        }
+    }
+}
