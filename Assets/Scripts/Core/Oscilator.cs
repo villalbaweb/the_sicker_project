6d1447587af4021180dfb18eb1100f493c4504a0
villalbaweb <villalbaweb@gmail.com>
2021-01-14 21:45:43 -0600

    Adding moving effect to Electricity Pickup prefab

---
+++ Assets/Scripts/Core/Oscilator.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace TheSicker.Core
+{
+    public class Oscilator : MonoBehaviour
+    {
+        // config
+        [Range(0, 1)]
+        [SerializeField] float movementFactor = 0;
+        [SerializeField] Vector3 movementVector = new Vector3();
+        [SerializeField] float period = 2f;
+
+        // state
+        const float tau = Mathf.PI * 2;
+        Vector3 startingPos;
+
+        // Start is called before the first frame update
+        void Awake()
+        {
+            startingPos = transform.position;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Oscilate();
+        }
+
+        private void Oscilate()
+        {
+            if (period <= Mathf.Epsilon) return;
+
+            float cycles = Time.time / period; // cycles keep growing over time
+            float rawSinWave = Mathf.Sin(cycles * tau); // based on cycles it will return value between -1 and 1
+            movementFactor = (rawSinWave + 1f) / 2f;    // recalculate the factor
+
+            Vector3 offset = movementVector * movementFactor;
+            transform.position = startingPos + offset;
+        }
+    }
+}
