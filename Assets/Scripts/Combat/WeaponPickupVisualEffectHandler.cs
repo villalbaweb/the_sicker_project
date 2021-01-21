9ac3d3f49a1768cff2f013d00d8ec55cc73188ee
villalbaweb <villalbaweb@gmail.com>
2021-01-20 20:45:43 -0600

    Adding WeaponPickupVisualEffectHandler script to weapon pickup prefab

---
+++ Assets/Scripts/Combat/WeaponPickupVisualEffectHandler.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace TheSicker.Combat
+{
+    public class WeaponPickupVisualEffectHandler : MonoBehaviour
+    {
+        // config
+        [SerializeField] ParticleSystem[] pickupParticleSystems = null;
+
+        public void VFXParticlesPlay()
+        {
+            print("Playing particle systems after pickup...");
+        }
+    }
+}
