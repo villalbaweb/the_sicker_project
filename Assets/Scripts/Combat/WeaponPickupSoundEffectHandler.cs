d7500d9c99ba9153b3e85a4b2bdc9525715b664f
villalbaweb <villalbaweb@gmail.com>
2021-02-07 22:17:47 -0600

    Adding WeaponPicupSoundEffectHandler script and proper sounds for electricity and laser weapon's pickup objects

---
+++ Assets/Scripts/Combat/WeaponPickupSoundEffectHandler.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace TheSicker.Combat
+{
+    public class WeaponPickupSoundEffectHandler : MonoBehaviour
+    {
+        // config
+        [SerializeField] AudioClip sfxAudio = null;
+        [SerializeField] float sfxAudioVolume = 0.5f;
+
+        public void PickupSoundEffectPlay()
+        {
+            if (!sfxAudio) return;
+
+            AudioSource.PlayClipAtPoint(sfxAudio, Camera.main.transform.position, sfxAudioVolume);
+        }
+    }
+}
