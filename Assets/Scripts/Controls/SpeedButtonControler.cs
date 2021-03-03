9b2b940fc1ca6ae4c178a158def4ec66f0395384
villalbaweb <villalbaweb@gmail.com>
2021-03-02 19:23:57 -0600

    Refactoring Game Controls prefab in order to avoid the direct dependency of use the event trigger in favor of create a new Monobehavior that implements the IPointerDownHandler, IPointerUpHandler interfaces and allow us to control dinamycally whats need to be controlled

---
+++ Assets/Scripts/Controls/SpeedButtonControler.cs
@@ -0,0 +1,29 @@
+﻿using TheSicker.Player;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace TheSicker.Controls
+{
+    public class SpeedButtonControler : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+    {
+        // cache
+        PlayerMover _playerMover;
+         
+        private void Awake()
+        {
+            _playerMover = FindObjectOfType<PlayerMover>();
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            _playerMover?.MoveBtnDown();
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            _playerMover.MoveBtnUp();
+        }
+
+
+    }
+}
