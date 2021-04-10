d3474db11a9cc5d15b84a7a0ce89de246703e8f8
villalbaweb <villalbaweb@gmail.com>
2021-04-10 13:37:23 -0500

    Add Follow On Distance Explosive Special Attack to damage player collision

---
+++ Assets/Scripts/Attacks/FollowOnDistanceExplosiveSpecialAttack.cs
@@ -0,0 +1,27 @@
+﻿using TheSicker.Core;
+using UnityEngine;
+
+namespace TheSicker.Attacks
+{
+    public class FollowOnDistanceExplosiveSpecialAttack : MonoBehaviour
+    {
+        // config
+        [SerializeField] int explosionDamage = 50;
+
+        // cache
+        DieHandler _dieHandler;
+
+        private void Awake() 
+        {
+            _dieHandler = GetComponent<DieHandler>();    
+        }
+
+        private void OnCollisionEnter2D(Collision2D other) 
+        {
+            if(other.gameObject.tag != "Player") return;
+
+            other.gameObject.GetComponent<Health>().TakeDamage(explosionDamage);
+            _dieHandler.Die();
+        }
+    }
+}
