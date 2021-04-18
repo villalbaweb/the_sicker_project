ab2d4eed6da77b22ec5c6dabd5563208abd7e0aa
villalbaweb <villalbaweb@gmail.com>
2021-04-18 18:33:46 -0500

    Refactor - Eliminate commented out code

--- Assets/Scripts/Attacks/FollowOnDistanceExplosiveSpecialAttack.cs
+++ Assets/Scripts/Attacks/FollowOnDistanceExplosiveSpecialAttack.cs
@@ -10,20 +10,11 @@ namespace TheSicker.Attacks
         [SerializeField] int explosionDamage = 50;
         [SerializeField] UnityEvent onExplosionAttack = null;
 
-        // cache
-        // DieHandler _dieHandler;
-
-        // private void Awake()
-        // {
-        //     _dieHandler = GetComponent<DieHandler>();
-        // }
-
         private void OnCollisionEnter2D(Collision2D other) 
         {
             if(other.gameObject.tag != "Player") return;
 
             other.gameObject.GetComponent<Health>().TakeDamage(explosionDamage);
-            //_dieHandler.Die();
             onExplosionAttack?.Invoke();    // this will invoke a few SFX
         }
     }
