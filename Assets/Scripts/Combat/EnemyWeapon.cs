b24b919256885c9c4d6ef92cc0cf841d08b47d9b
villalbaweb <villalbaweb@gmail.com>
2021-05-19 01:34:56 -0500

    Refactor EnemyWeapon scriptable object

--- Assets/Scripts/Combat/EnemyWeapon.cs
+++ Assets/Scripts/Combat/EnemyWeapon.cs
@@ -1,20 +1,17 @@
-﻿using UnityEngine;
+﻿using TheSicker.Core;
+using UnityEngine;
 
 namespace TheSicker.Combat
 {
     [CreateAssetMenu(fileName = "EnemyWeapon", menuName = "Weapons/Make New Enemy Weapon", order = 1)]
     public class EnemyWeapon : ScriptableObject
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-            
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-            
-        }
+        // config
+        [SerializeField] ObjectPoolIds projectile = ObjectPoolIds.EnemyBlueProjectile;
+        [SerializeField] float projectileFiringPeriod = 0.1f;
+        [SerializeField] AudioClip onFireSoundClip = null;
+        [SerializeField] [Range(0, 1)] float fireSoundVolume = 0.5f;
+        [SerializeField] float projectileDistance = 20f;
+        [SerializeField] ParticleSystem muzzlerParticleSystemPrefab = null;
     }
 }
