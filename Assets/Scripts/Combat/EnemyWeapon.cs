2b9ed0b3d2f3ba8689c63f2ddaa00718ea4cda5c
villalbaweb <villalbaweb@gmail.com>
2021-05-19 01:29:14 -0500

    Add EnemyWeapon scriptable object

---
+++ Assets/Scripts/Combat/EnemyWeapon.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace TheSicker.Combat
+{
+    [CreateAssetMenu(fileName = "EnemyWeapon", menuName = "Weapons/Make New Enemy Weapon", order = 1)]
+    public class EnemyWeapon : ScriptableObject
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
