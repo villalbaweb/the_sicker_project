b7e17182fc460cccdff929cd7a2feb1c8e364d5e
villalbaweb <villalbaweb@gmail.com>
2021-10-30 16:27:51 -0500

    Refactoring, PlayerMaxXpHandler to keep track of max xp points whenever has been reached and save back to file when player dies

--- Assets/Scripts/Player/PlayerMaxXpHandler.cs
+++ Assets/Scripts/Player/PlayerMaxXpHandler.cs
@@ -1,4 +1,5 @@
 using TheSicker.SaveSystem;
+using TheSicker.Stats;
 using UnityEngine;
 
 namespace TheSicker.Player
@@ -6,19 +7,59 @@ namespace TheSicker.Player
     public class PlayerMaxXpHandler : MonoBehaviour, ISaveableComponent
     {
         // config
-        [SerializeField] int maxXpLevel = 10;
+        [SerializeField] float maxXpLevel = 10;
 
         // cache
         SaveSystemWrapper _saveSystemWrapper;
+        Experience _experience;
+
+        // state
+        bool isMaxXpLevelReached = false;
 
         private void Awake()
         {
             _saveSystemWrapper = FindObjectOfType<SaveSystemWrapper>();
+            _experience = GetComponent<Experience>();
         }
 
         private void Start()
         {
             _saveSystemWrapper?.GetMaxXpPoint();
+        }
+
+        private void OnEnable()
+        {
+            if (_experience)
+            {
+                _experience.OnExperienceGainedEvent += OnExperienceGained;
+            }
+        }
+
+        private void OnDisable()
+        {
+            if (_experience)
+            {
+                _experience.OnExperienceGainedEvent -= OnExperienceGained;
+            }
+        }
+
+        public void MaxXpOnPlayerDieSave()
+        {
+            if (!isMaxXpLevelReached) return;
+
+            _saveSystemWrapper.SaveMaxXpPoint();
+        }
+
+
+        private void OnExperienceGained()
+        {
+            float currentXP = _experience.GetExperience();
+            isMaxXpLevelReached = currentXP >= maxXpLevel;
+
+            if (isMaxXpLevelReached)
+            {
+                maxXpLevel = currentXP;
+            }
         }
 
         #region ISaveableComponent Implementation
@@ -34,7 +75,7 @@ namespace TheSicker.Player
         {
             if (stateType != StateType.MaxXp) return;
 
-            maxXpLevel = (int)state;
+            maxXpLevel = (float)state;
         }
 
         #endregion
