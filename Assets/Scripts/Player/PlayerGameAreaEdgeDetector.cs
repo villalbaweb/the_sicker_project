b12761179e22724cbbf5e0d9d291877ba8a83fc2
villalbaweb <villalbaweb@gmail.com>
2021-11-13 15:47:42 -0600

    Refactoring, optimize the way we calculate edge aread detector by using trigger collider instead of raycasting

--- Assets/Scripts/Player/PlayerGameAreaEdgeDetector.cs
+++ Assets/Scripts/Player/PlayerGameAreaEdgeDetector.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Events;
 
 namespace TheSicker.Player
@@ -7,61 +6,13 @@ namespace TheSicker.Player
     public class PlayerGameAreaEdgeDetector : MonoBehaviour
     {
         // config
-        [SerializeField] LayerMask edgeLayers = new LayerMask();
-        [SerializeField] float rayCastDistance = 1f;
         [SerializeField] UnityEvent OnEdgeDetected = new UnityEvent();
 
-        // state
-        List<Vector2> directionsToCheck;
-
-        private void Awake()
+        private void OnTriggerEnter2D(Collider2D other)
         {
-            directionsToCheck = new List<Vector2>
-            {
-                transform.right,
-                (transform.up + transform.right).normalized,
-                transform.up,
-                (transform.up - transform.right).normalized,
-                -transform.right,
-                (-transform.up - transform.right).normalized,
-                -transform.up,
-                (transform.right - transform.up).normalized
-            };
+            if (other.tag != "Playable Area Delimiter") return;
+
+            OnEdgeDetected?.Invoke();
         }
-
-        private void FixedUpdate()
-        {
-            EdgeDetectionMechanism();
-        }
-
-        private void EdgeDetectionMechanism()
-        {
-            foreach(Vector2 direction in directionsToCheck)
-            {
-                if(HasEdgeCollission(direction))
-                {
-                    return;
-                }
-            }
-        }
-
-        private bool HasEdgeCollission(Vector2 direction)
-        {
-            RaycastHit2D raycastHit = Physics2D.Raycast(transform.position, direction, rayCastDistance, edgeLayers);
-
-            if (raycastHit.collider != null && IsInLayerMask(raycastHit.collider.gameObject.layer, edgeLayers))
-            {
-                OnEdgeDetected?.Invoke();
-                return true;
-            }
-
-            return false;
-        }
-
-        private bool IsInLayerMask(int layer, LayerMask layermask)
-        {
-            return layermask == (layermask | (1 << layer));
-        }
-
     }
 }
