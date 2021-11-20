7293abfdf4ccbd9b1b018a971cd54193cdf8bb17
villalbaweb <villalbaweb@gmail.com>
2021-11-20 08:42:03 -0600

    Refactoring, remove text game objects for Start/Exit buttons and add SplashSceneController

---
+++ Assets/Scripts/UI/SplashSceneExitButtonHandler.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace TheSicker.UI
+{
+    public class SplashSceneExitButtonHandler : MonoBehaviour
+    {
+        public void ExitButtonPressed()
+        {
+            print("Exit button pressed...");
+        }
+    }
+}
---
+++ Assets/Scripts/UI/SplashSceneStartButtonHandler.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace TheSicker.UI
+{
+    public class SplashSceneStartButtonHandler : MonoBehaviour
+    {
+        public void StartButtonPressed()
+        {
+            print("Start button pressed...");
+        }
+    }
+}
