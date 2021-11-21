386771431b5a3b709b1ab2aefb9372af1abdf9a6
villalbaweb <villalbaweb@gmail.com>
2021-11-20 22:43:58 -0600

    Refactoring, caching the Main Camera access for PLayerInScreenHandler

--- Assets/Scripts/Player/PlayerInScreenHandler.cs
+++ Assets/Scripts/Player/PlayerInScreenHandler.cs
@@ -4,12 +4,19 @@ namespace TheSicker.Player
 {
     public class PlayerInScreenHandler : MonoBehaviour
     {
+        // cache
+        Camera _mainCamera;
+
+        private void Awake()
+        {
+            _mainCamera = Camera.main;
+        }
         void Update()
         {
-            Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+            Vector3 pos = _mainCamera.WorldToViewportPoint(transform.position);
             pos.x = Mathf.Clamp01(pos.x);
             pos.y = Mathf.Clamp01(pos.y);
-            transform.position = Camera.main.ViewportToWorldPoint(pos);
+            transform.position = _mainCamera.ViewportToWorldPoint(pos);
         }
     }
 }
