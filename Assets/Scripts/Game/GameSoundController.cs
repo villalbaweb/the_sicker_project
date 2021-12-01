710199def29770d143302b6817b837410fdb81f3
villalbaweb <villalbaweb@gmail.com>
2021-12-01 08:46:31 -0600

    Adding, GameSoundController script to centralize the use of PlayClipAtPoint

---
+++ Assets/Scripts/Game/GameSoundController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+namespace TheSicker.Player
+{
+    public class GameSoundController : MonoBehaviour
+    {
+        // config params
+        [SerializeField] float audioVolume = 1f;
+
+        // state
+        public bool IsSoundMute
+        {
+            get { return _isSoundMute; }
+        }
+        private bool _isSoundMute = false;
+
+        public void SwitchSoundMute(bool isMuted)
+        {
+            print($"Set sound {isMuted}");
+            _isSoundMute = isMuted;
+        }
+
+        public void PlayClipAtCamera(AudioClip clip, float specificVolume = -1)
+        {
+            if (!clip || _isSoundMute) return;
+
+            float playSoundVolume = specificVolume == -1 ? audioVolume : specificVolume;
+
+            AudioSource.PlayClipAtPoint(clip, Camera.main.transform.position, playSoundVolume);
+        }
+    }
+}
