5c75e4550ced205b67e82b368d7e68131bdc4168
villalbaweb <villalbaweb@gmail.com>
2022-01-12 21:24:35 -0600

    Adding, the DirectionalDamageSpawner component to Follow On Distance Attacker prefab

--- Assets/Scripts/Core/DirectionalDamageSpawner.cs
+++ Assets/Scripts/Core/DirectionalDamageSpawner.cs
@@ -4,9 +4,24 @@ namespace TheSicker.Core
 {
     public class DirectionalDamageSpawner : MonoBehaviour, IDirectionalDamageSpawner
     {
+        // config
+        [SerializeField] ObjectPoolIds popUpObjectPoolId;
+
+        // cache
+        ObjectPooler _objectPooler;
+
+        private void Awake()
+        {
+            _objectPooler = FindObjectOfType<ObjectPooler>();
+        }
+
+        #region Interface Implementation 
+
         public void DirectionalDamageVfxSpawn(Vector2 position)
         {
-            print($"DirectionalDamageVfxSpawn {position}");
+            GameObject popUpGameObject = _objectPooler.SpawnFromPool(popUpObjectPoolId.ToString(), position, Quaternion.identity);
         }
+
+        #endregion
     }
 }
