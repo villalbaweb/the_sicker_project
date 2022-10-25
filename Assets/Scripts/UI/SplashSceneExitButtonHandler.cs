aa540b094104692bc75d845ee524e09f4445e911
Daniel Villalba <villalbaweb@gmail.com>
2022-10-25 09:59:08 -0500

    Adding, sound to Canvas Splash Scene buttons

--- Assets/Scripts/UI/SplashSceneExitButtonHandler.cs
+++ Assets/Scripts/UI/SplashSceneExitButtonHandler.cs
@@ -1,11 +1,33 @@
+using System.Collections;
+using TheSicker.Game;
 using UnityEngine;
 
 namespace TheSicker.UI
 {
     public class SplashSceneExitButtonHandler : MonoBehaviour
     {
+        // config
+        [SerializeField] AudioClip _clip;
+
+        // cache
+        GameSoundController _gameSoundController;
+
+        private void Awake()
+        {
+            _gameSoundController = FindObjectOfType<GameSoundController>();
+        }
+
         public void ExitButtonPressed()
         {
+            StartCoroutine(ExitWithSound());
+        }
+
+        private IEnumerator ExitWithSound()
+        {
+            _gameSoundController.PlayClipAtCamera(_clip);
+
+            yield return new WaitForSeconds(2.0f);
+
             Application.Quit();
         }
     }
