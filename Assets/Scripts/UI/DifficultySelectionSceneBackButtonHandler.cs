e2babe229d415dfbd5919c13833d542d66f7c52b
Daniel Villalba <villalbaweb@gmail.com>
2022-10-27 09:33:36 -0500

    Refactoring, Setup scene to use generic buttons instead

--- Assets/Scripts/UI/DifficultySelectionSceneBackButtonHandler.cs
+++ Assets/Scripts/UI/DifficultySelectionSceneBackButtonHandler.cs
@@ -5,23 +5,17 @@ namespace TheSicker.UI
 {
     public class DifficultySelectionSceneBackButtonHandler : MonoBehaviour
     {
-        // config
-        [SerializeField] AudioClip _clip;
-
         // cache
         GameSceneController _gameSceneController;
-        GameSoundController _gameSoundController;
 
         private void Awake()
         {
             _gameSceneController = FindObjectOfType<GameSceneController>();
-            _gameSoundController = FindObjectOfType<GameSoundController>();
         }
 
         public void BackButtonPressed()
         {
             _gameSceneController.LoadMainMenu();
-            _gameSoundController.PlayClipAtCamera(_clip);
         }
     }
 }
--- Assets/Scripts/UI/DifficultySelectionSceneStartButtonHandler.cs
+++ Assets/Scripts/UI/DifficultySelectionSceneStartButtonHandler.cs
@@ -5,23 +5,17 @@ namespace TheSicker.UI
 {
     public class DifficultySelectionSceneStartButtonHandler : MonoBehaviour
     {
-        // config
-        [SerializeField] AudioClip _clip;
-
         // cache
         GameSceneController _gameSceneController;
-        GameSoundController _gameSoundController;
 
         private void Awake()
         {
             _gameSceneController = FindObjectOfType<GameSceneController>();
-            _gameSoundController = FindObjectOfType<GameSoundController>();
         }
 
         public void StartButtonPressed()
         {
             _gameSceneController.LoadPlayLevel();
-            _gameSoundController.PlayClipAtCamera(_clip);
         }
     }
 }
