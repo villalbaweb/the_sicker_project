279e5f10d1dab6b40df742e15c3a3540756d0b2d
Daniel Villalba <villalbaweb@gmail.com>
2022-11-13 17:18:23 -0600

    Refactoring, adding RewardManager script with some wirings to player and menu controller, still need to reconfigure the player

--- Assets/Scripts/AdSystem/RewardManager.cs
+++ Assets/Scripts/AdSystem/RewardManager.cs
@@ -1,11 +1,31 @@
+using TheSicker.Core;
+using TheSicker.Menu;
+using TheSicker.Player;
 using UnityEngine;
 
 namespace TheSicker.AdSystem
 {
     public class RewardManager : MonoBehaviour
     {
+        // cache
+        PlayerAdsManager _playerAdsManager;
+        GameMenuController _gameMenuController;
+
+        private void Awake()
+        {
+            _playerAdsManager = FindObjectOfType<PlayerAdsManager>();
+            _gameMenuController = FindObjectOfType<GameMenuController>();
+        }
+
         public void GrandReward()
         {
+            if (!_playerAdsManager) return;
+
+            _gameMenuController?.SetAdsMenuEnabled(false);
+
+            _playerAdsManager.gameObject.SetActive(true);
+            _playerAdsManager.GetComponent<Health>().RestoreInitialHealth();
+
             print("Reward granted!!!");
         }
     }
