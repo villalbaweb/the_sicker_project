8b26f91d338ed079ad934602f61b192a88ad1e29
Daniel Villalba <villalbaweb@gmail.com>
2022-11-13 15:31:38 -0600

    Adding, PlayerAddsmanager that will show Ads button when die

---
+++ Assets/Scripts/Player/PlayerAdsManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace TheSicker.Player
+{
+    public class PlayerAdsManager : MonoBehaviour
+    {
+        // state
+        bool IsAdsUsed = false;
+
+        public void DisplayAdsButtonOnDie()
+        {
+            print("Here to display the OnDie Ads menu...");
+        }
+    }
+}
