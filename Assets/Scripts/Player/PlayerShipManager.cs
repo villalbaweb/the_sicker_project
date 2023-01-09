b36c2a0d7046c105b173fc4def26e268172a2808
Daniel Villalba <villalbaweb@gmail.com>
2023-01-09 16:45:26 -0600

    Refactoring, now the MiniMap Icon changes accordingly to the randomly generated ship

--- Assets/Scripts/Player/PlayerShipManager.cs
+++ Assets/Scripts/Player/PlayerShipManager.cs
@@ -10,17 +10,29 @@ namespace TheSicker.Player
 
         private void Awake()
         {
-            Ships selectedShip = RandmlySelectShip();
+            Ships selectedShip = RandomlySelectShip();
 
             print($"{selectedShip.Name}");
+
+            MiniMapIconSetup(selectedShip);
         }
 
-        private Ships RandmlySelectShip()
+        private Ships RandomlySelectShip()
         {
             return playerShips.Ships
             .OrderBy(x => System.Guid.NewGuid())
             .Take(1)
             .FirstOrDefault();
+        }
+
+        private void MiniMapIconSetup(Ships generatedShip)
+        {
+            SpriteRenderer spriteRenderer = transform.Find("Minimap Icon")?.GetComponent<SpriteRenderer>();
+
+            if (spriteRenderer != null)
+            {
+                spriteRenderer.sprite = generatedShip.MinimapIcon;
+            }
         }
     }
 }
